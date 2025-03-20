b18420fa7cf19e844b98b7afc1a79ab5031b7595
Wuttichai 022 <674259022@webmail.npru.ac.th>
2025-03-20 10:14:24 +0700

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
-        System.out.print("num:"); // แสดงข้อความให้ผู้ใช้ป้อนตัวเลข
-        int num = scanner.nextInt(); // รับค่าตัวเลขที่ผู้ใช้ป้อนและเก็บในตัวแปร num
-        long factorial = 1; // กำหนดตัวแปร factorial เริ่มต้นที่ 1 (เพราะการคูณกับ 1 ไม่ทำให้ค่าผลลัพธ์เปลี่ยนแปลง)
+        System.out.print("num:"); 
+        int num = scanner.nextInt(); 
+        long factorial = 1; 
 
         // ใช้ for loop เพื่อคำนวณค่า factorial
         for (int i = 1; i <= num; i++) { 
-            factorial *= i; // คูณค่าปัจจุบันของ i กับตัวแปร factorial แล้วเก็บค่าผลลัพธ์กลับไปที่ factorial
+            factorial *= i; 
         }
 
-        // แสดงผลลัพธ์ค่า 
+        
         System.out.println("Factorial  " + num + " is " + factorial);
 
         scanner.close(); // ปิด Scanner                                         
